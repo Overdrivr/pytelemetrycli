76d7efa4b31f08ee3144cc9b729bf8c2a30a8462
Rémi Bèges <remi.beges@gmail.com>
2016-01-26 14:31:37 +0100

    runner class
    To manage and continuously update the serial port

---
+++ runner.py
@@ -0,0 +1,39 @@
+import threading
+import time
+# Main class
+class Runner:
+    def __init__(self, transport, telemetry):
+
+        self.transport = transport
+        self.telemetryWrapper = telemetry
+
+        self.running = threading.Event()
+        self.running.set()
+
+        self.connected = threading.Event()
+        self.connected.clear()
+
+    def connect(self,options):
+        print("Connecting to...",options)
+        self.transport.connect(options)
+        self.connected.set()
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+    def disconnect(self):
+        self.connected.clear()
+        self.transport.disconnect()
+
+    def terminate(self):
+        self.running.clear()
+        try:
+            self.transport.disconnect()
+        except:
+            pass # Already disconnected
+
+    def run(self):
+        while self.running.is_set():
+            if self.connected.is_set():
+                self.telemetryWrapper.update()
+            else:
+                time.sleep(0.5)
