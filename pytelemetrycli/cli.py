10aa82fb1167892ce69c306a01c87093c69e7ba6
Rémi Bèges <remi.beges@gmail.com>
2016-02-04 22:43:40 +0100

    Implemented full interface

--- pytelemetrycli/cli.py
+++ pytelemetrycli/cli.py
@@ -3,28 +3,19 @@ import sys
 import cmd
 from docopt import docopt, DocoptExit
 
-
 def docopt_cmd(func):
-    """
-    This decorator is used to simplify the try/except block and pass the result
-    of the docopt parsing to the called action.
-    """
     def fn(self, arg):
         try:
             opt = docopt(fn.__doc__, arg)
 
         except DocoptExit as e:
-            # The DocoptExit is thrown when the args do not match.
-            # We print a message to the user and the usage block.
-
-            print('Invalid Command!')
+            print('Command is invalid. See :')
             print(e)
             return
 
         except SystemExit:
             # The SystemExit exception prints the usage for --help
             # We do not need to do the print here.
-
             return
 
         return func(self, opt)
@@ -34,8 +25,7 @@ def docopt_cmd(func):
     fn.__dict__.update(func.__dict__)
     return fn
 
-
-class MyInteractive (cmd.Cmd):
+class Application (cmd.Cmd):
     intro = 'pytelemetry terminal started.' \
           + ' (type help for a list of commands.)'
     prompt = ':> '
@@ -44,25 +34,75 @@ class MyInteractive (cmd.Cmd):
     @docopt_cmd
     def do_serial(self, arg):
         """
-            Usage: serial <port> [options]
+Connects pytelemetry to the serial port.
 
-            Options:
-            -b X, --bauds X        Connection speed in bauds  [default: 9600]
+Usage: serial <port> [options]
+
+Options:
+-b X, --bauds X        Connection speed in bauds  [default: 9600]
+
         """
+        print(arg)
 
+    @docopt_cmd
+    def do_print(self, arg):
+        """
+Prints X last received samples from <topic>.
+
+Usage: print <topic> [options]
+
+Options:
+-a X, --amount X        Amount of samples to display [default: 1]
+
+        """
+        print(arg)
+
+    @docopt_cmd
+    def do_ls(self, arg):
+        """
+Prints a list of all received topics.
+
+Usage: ls
+        """
+        print(arg)
+
+    @docopt_cmd
+    def do_plot(self, arg):
+        """
+Plots <topic> in a graph window.
+
+Usage: plot <topic>
+        """
+        print(arg)
+
+    @docopt_cmd
+    def do_pub(self, arg):
+        """
+Publishes a (value | string) on <topic>.
+
+Usage: pub <topic> <value> (-ui8 | -ui16 | -ui32 | -i8 | -i16 | -i32 | -f32 | -s)
+        """
+        print(arg)
+
+    @docopt_cmd
+    def do_disconnect(self, arg):
+        """
+Disconnects from any open connection.
+
+Usage: disconnect
+        """
         print(arg)
 
     def do_quit(self, arg):
         """
-            Quits out of Interactive Mode.
+Quits out of Interactive Mode.
         """
 
         print('Good Bye!')
         exit()
 
-
 try:
-    MyInteractive().cmdloop()
+    Application().cmdloop()
 except SystemExit:
     pass
 except KeyboardInterrupt:
