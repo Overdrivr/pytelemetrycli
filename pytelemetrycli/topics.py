31f45a0615562e3431608eb63e8114870e565022
Rémi Bèges <remi.beges@gmail.com>
2016-02-07 14:03:12 +0100

    Topics can now transfer data received on a topic to a given queue
    data is put in a queue, that is read on the other side by pyqtgraph
    process

--- pytelemetrycli/topics.py
+++ pytelemetrycli/topics.py
@@ -1,11 +1,16 @@
 class Topics:
     def __init__(self):
         self.topics = dict()
+        self.transfers = dict()
 
     def process(self,topic, payload):
         if not topic in self.topics:
             self.topics[topic] = []
         self.topics[topic].append(payload)
+        # If there is an active transfer, transfer received data to the queue
+        if topic in self.transfers:
+            self.transfers[topic]['queue'].put([self.transfers[topic]['lastindex'], payload])
+            self.transfers[topic]['lastindex'] += 1
 
     def ls(self):
         return self.topics.keys()
@@ -24,3 +29,15 @@ class Topics:
 
     def exists(self,topic):
         return topic in self.topics
+
+    def transfer(self,topic,queue):
+        # If the topic data is not already transfered to some queue
+        if not topic in self.transfers:
+            self.transfers[topic] = dict()
+            self.transfers[topic]['queue'] = queue
+            self.transfers[topic]['lastindex'] = 0
+            # If there is already existing data under the topic
+            if topic in self.topics:
+                for item in self.topics[topic]:
+                    queue.put([self.transfers[topic]['lastindex'], item])
+                    self.transfers[topic]['lastindex'] += 1
