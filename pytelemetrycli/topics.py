f2856a45ec0e9179c2cb07d47928bc5e480ab628
Rémi Bèges <remi.beges@gmail.com>
2016-02-15 19:14:32 +0100

    indexed data support in Topics

--- pytelemetrycli/topics.py
+++ pytelemetrycli/topics.py
@@ -1,16 +1,36 @@
+from sortedcontainers import SortedDict
+
 class Topics:
     def __init__(self):
         self.topics = dict()
         self.transfers = dict()
 
-    def process(self,topic, payload):
+    def process(self,topic, payload, options=None):
         if not topic in self.topics:
-            self.topics[topic] = []
-        self.topics[topic].append(payload)
+            self.topics[topic] = dict()
+            self.topics[topic]['raw'] = [] # For raw data
+            self.topics[topic]['xy'] = SortedDict() # For indexed data
+            if options:
+                self.topics[topic]['type'] = 'indexed'
+            else:
+                self.topics[topic]['type'] = 'linear'
+
+        t = self.topics[topic]
+        t['raw'].append(payload)
+
+        # If topic is of type indexed data, also store in ordered dict
+        if options:
+            t['xy'][options['index']] = payload
+
         # If there is an active transfer, transfer received data to the queue
         if topic in self.transfers:
-            self.transfers[topic]['queue'].put([self.transfers[topic]['lastindex'], payload])
-            self.transfers[topic]['lastindex'] += 1
+            # For indexed data, provide the index for x and payload for y
+            if t['type'] == 'indexed' and options is not None:
+                self.transfers[topic]['queue'].put([options['index'], payload])
+            # For linear data, provide sample id for x and payload for y
+            elif t['type'] == 'linear':
+                self.transfers[topic]['queue'].put([self.transfers[topic]['lastindex'], payload])
+                self.transfers[topic]['lastindex'] += 1
 
     def ls(self):
         return self.topics.keys()
@@ -19,13 +39,15 @@ class Topics:
         if not topic in self.topics:
             return None
         if amount == 0 or amount is None:
-            return self.topics[topic]
-        return self.topics[topic][-amount:]
+            return self.topics[topic]['raw']
+        # TODO : Print indexed data ?
+        return self.topics[topic]['raw'][-amount:]
 
     def count(self,topic):
         if not topic in self.topics:
             return 0
-        return len(self.topics[topic])
+        # TODO : what to do on indexed data ?
+        return len(self.topics[topic]['raw'])
 
     def exists(self,topic):
         return topic in self.topics
@@ -38,6 +60,10 @@ class Topics:
             self.transfers[topic]['lastindex'] = 0
             # If there is already existing data under the topic
             if topic in self.topics:
-                for item in self.topics[topic]:
-                    queue.put([self.transfers[topic]['lastindex'], item])
-                    self.transfers[topic]['lastindex'] += 1
+                if self.topics[topic]['type'] == 'indexed':
+                    for key, value in self.topics[topic]['xy'].iteritems():
+                        queue.put([key, value])
+                elif self.topics[topic]['type'] == 'linear':
+                    for item in self.topics[topic]['raw']:
+                        queue.put([self.transfers[topic]['lastindex'], item])
+                        self.transfers[topic]['lastindex'] += 1
